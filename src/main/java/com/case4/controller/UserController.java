0763bdb27da6e061d25ca6c8d87d70a79c6cfd80
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-07-08 08:47:05 +0700

    login

--- src/main/java/com/case4/controller/UserController.java
+++ src/main/java/com/case4/controller/UserController.java
@@ -8,10 +8,11 @@ import com.case4.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+@Controller
 @RestController
 @RequestMapping("/users")
 public class UserController {
