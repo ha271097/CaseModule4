20e6aed7f6f7d756883ef5bcb3e798753f2e2e95
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-07-10 23:08:56 +0700

    push buy order , chưa xong, mới set cứng dữ liệu chưa có nhập

--- src/main/java/com/case4/model/Order.java
+++ src/main/java/com/case4/model/Order.java
@@ -32,6 +32,22 @@ public class Order {
         this.quantity = quantity;
     }
 
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/case4/repo/IOderRepo.java
+++ src/main/java/com/case4/repo/IOderRepo.java
@@ -1,11 +1,20 @@
 package com.case4.repo;
 
 import com.case4.model.Order;
+import com.case4.model.Product;
 import com.case4.model.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOderRepo extends JpaRepository<Order,Long> {
     Iterable<Order> getAllByUser(User user);
+
+    void deleteAllByUser(User user);
+
+    @Query(
+            value = "delete from orders where product_id = :product.id  and user_id = :user.id"
+    )
+    void removeByProductAndUser(Long id_product, Long id_user);
 }
